8a26340a12fd3d6e645f467fdabf6b95645f1aff
TJReynolds88 <tj.reynolds1988@gmail.com>
2020-06-20 22:55:04 -0500

    Add files via upload

--- websockets-server.js
+++ websockets-server.js
@@ -5,14 +5,23 @@ var ws = new WebSocketServer ({
     port: port
 });
 
+var message = [];
+
 console.log('websockets server started');
 
 ws.on('connection', function (socket){
     console.log('client connection established');
 
+    message.forEach(function (msg) {
+        socket.send(msg);
+    });
+
     socket.on('message', function (data) {
         console.log('message recived:' + data);
-        socket.send(data);
+        message.push(data);
+        ws.clients.forEach(function (clientSocket) {
+            clientSocket.send(data)
+        });
         
     });
 });
